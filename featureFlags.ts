841ccb21bdc0d2646401b86594ec6b20b8c568ff
Hemanth Surya <hemanth@triorama.ai>
2025-11-25 12:10:25 +0000

    init feature flag commit

---
+++ featureFlags.ts
@@ -0,0 +1,24 @@
+export const FeatureFlags = [
+    
+  {
+    _id: 'enableFlaps20251119',
+    title: 'Enable Flaps on the wings',
+    defaultValue: 0,
+    description: 'Enables Flaps on the wings for drag control',
+    tickets: ['F-14'],
+  },
+  {
+    _id: 'enableLandingGear20251119',
+    title: 'Enable Landing Gear',
+    defaultValue: 0,
+    description: 'When enabled, you can land your metal bird on the tarmac (hopefully)',
+    tickets: ['F-16'],
+  },
+  {
+    _id: 'enableCanopy20251120',
+    title: 'Enable Canopy',
+    defaultValue: 0,
+    description: 'When enabled, a sturdy, transparent canopy lowers over the cockpit',
+    tickets: ['F-22'],
+  },
+]
